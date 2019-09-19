4cd17cdbcf0a2361292ce115580fa84424e8ee01
Salil V Nair <salilvnair@icloud.com>
2019-09-19 09:40:06 +0530

    added ref to expansion button

--- src/lib/components/expansion-panel/expansion-panel.component.jsx
+++ src/lib/components/expansion-panel/expansion-panel.component.jsx
@@ -20,8 +20,13 @@ export class ExpansionPanel extends React.Component {
     render() {
         return (
             <div>
-                    <button disabled={this.props.disabled ? true : false} className="accordion" onClick={(e) => this.expandCollapsePanel(e)}>{this.props.header?this.props.header:''}</button>
-                    <div className="panel">
+                    <button  
+                        ref={this.props.ref}
+                        disabled={this.props.disabled ? true : false} className="accordion" 
+                        onClick={(e) => this.expandCollapsePanel(e)}>{this.props.header?this.props.header:''}
+                    </button>
+                    <div 
+                        className="panel">
                         {this.props.children?this.props.children:(this.props.content?this.props.content:'')}
                     </div>
             </div>
